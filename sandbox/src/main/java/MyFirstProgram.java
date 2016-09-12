9f876a77c0831ed915d6e4f8a9e4592c54204e6c
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-09-13 01:33:52 +0300

    Тестовое задание №2. Создан класс Piont и 2 функции вычисления расстояния между 2-я точками

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,21 @@
 public class MyFirstProgram{
 	public static void main(String[] args){
-		System.out.println("Hello.");	
-	}	
+
+		Point point1 = new Point(12, 10);
+		Point point2 = new Point(6, 7);
+
+		//реализация через функцию вне класса
+		double result1 = distance(point1, point2);
+		System.out.println("Расстояние между точками т1 ("+ point1.x+","+point1.y +") и т2 ("+ point2.x+","+ point2.y +") = "+  result1);
+		// реализация в классе
+		double result2 = point1.distanceTo(point2);
+		System.out.println("Расстояние между точками т1 ("+ point1.x+","+point1.y +") и т2 ("+ point2.x+","+ point2.y +") = "+  result2);
+	}
+	public static double distance(Point p1, Point p2)	{
+		int n = (p1.x - p2.x);
+		int m = (p1.y - p2.y);
+
+		return  Math.sqrt(n*n + m*m);
+	}
 }
 
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,17 @@
+public class Point {
+
+    public int x;
+    public int y;
+
+    public  Point (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point point) {
+        int n = (this.x - point.x);
+		int m = (this.y - point.y);
+
+		return  Math.sqrt(n*n + m*m);
+    }
+}
