ae874d137d4b14c245e1c20806d4df812b327470
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-09-26 00:27:39 +0300

    Тестовое задание №4.  Создан Тест на проверку создания контакта с выделением методов и параметаризацией метода заполнения форм

---
+++ addressbook-web-tests/src/test/java/ru/autoqa/addressbook/ContactCreationTest.java
@@ -0,0 +1,96 @@
+package ru.autoqa.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ContactCreationTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String userName, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(userName);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void ContactCreationTest() {
+
+        goToCreateContactPage();
+        fillContactForm(new ContactData("testFirstName", "testLastName", "testMiddleName", "testAddressString", "79991001010", "test@test.ru", "1999"));
+        submitContactCreation();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getTestFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getTestLastName());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getTestMiddleName());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getTestAddressString());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).click();
+        }
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).click();
+        }
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(contactData.getYear());
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[4]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[4]")).click();
+        }
+
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void goToCreateContactPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/autoqa/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.autoqa.addressbook;
+
+public class ContactData {
+    private final String testFirstName;
+    private final String testLastName;
+    private final String testMiddleName;
+    private final String testAddressString;
+    private final String mobilePhone;
+    private final String email;
+    private final String year;
+
+    public ContactData(String testFirstName, String testLastName, String testMiddleName, String testAddressString, String mobilePhone, String email, String year) {
+        this.testFirstName = testFirstName;
+        this.testLastName = testLastName;
+        this.testMiddleName = testMiddleName;
+        this.testAddressString = testAddressString;
+        this.mobilePhone = mobilePhone;
+        this.email = email;
+        this.year = year;
+    }
+
+    public String getTestFirstName() {
+        return testFirstName;
+    }
+
+    public String getTestLastName() {
+        return testLastName;
+    }
+
+    public String getTestMiddleName() {
+        return testMiddleName;
+    }
+
+    public String getTestAddressString() {
+        return testAddressString;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
