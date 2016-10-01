049c6600ca66357fc21fa90b18e5cb650ed264c3
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-10-01 20:49:19 +0300

    Тестовое задание №7.  Добавление класса Equation и тестов к методу

---
+++ sandbox/src/test/java/ru/autoqa/sandbox/EquationTests.java
@@ -0,0 +1,37 @@
+package ru.autoqa.sandbox;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+    @Test
+    public void test() {
+        Equation e = new Equation(1, 1, 1);
+        Assert.assertEquals(e.rootNumber(),0);
+    }
+    @Test
+    public void test1() {
+        Equation e = new Equation(1, 2, 1);
+        Assert.assertEquals(e.rootNumber(),1);
+    }
+    @Test
+    public void test2() {
+        Equation e = new Equation(1, 5, 6);
+        Assert.assertEquals(e.rootNumber(),2);
+    }
+    @Test
+    public void test3() {
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+    @Test
+    public void test4() {
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNumber(),0);
+    }
+    @Test
+    public void test5() {
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNumber(),-1);
+    }
+}
