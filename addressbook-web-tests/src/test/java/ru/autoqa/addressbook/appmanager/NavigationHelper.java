a22b7131ecc89ce1a88e7d7ac99c3fdd48fec97f
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-10-05 22:53:55 +0300

    Тестовое задание № 8.  Проведена оптимизация кода. Проверка нахождения на странице

--- addressbook-web-tests/src/test/java/ru/autoqa/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/addressbook/appmanager/NavigationHelper.java
@@ -11,17 +11,27 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void goToGroupsPage() {
-
+    if (isElementPresent(By.tagName("h1"))
+        && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+        && isElementPresent(By.tagName("new"))){
+            return;
+        }
         click(By.linkText("groups"));
+
     }
 
     public void goToCreateContactPage() {
-
+    if (isElementPresent(By.tagName("h1"))
+        && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")){
+        return;
+    }
         click(By.linkText("add new"));
     }
 
     public void goToContactListPage() {
-
+    if (isElementPresent(By.id("maintable"))){
+        return;
+    }
         click(By.linkText("home"));
     }
 }
