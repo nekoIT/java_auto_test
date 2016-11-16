1518af56125498c8e5a6172bb4f7f3f7f0ad2fc5
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-11-16 22:06:54 +0300

    Тестовое задание № 20 Реализован тест для смены пароля в MantisBT

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname	= 'localhost';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_database_name = 'bugtracker';
+$g_db_schema = '';
+$g_db_type = 'mysqli';
+$g_signup_use_captcha	= OFF;
+$g_crypto_master_salt     = 'KcuAoKph1PH19DYsdyQ7Xx0ojQm5PQGpFL60id52wbw=';
+$g_phpMailer_method = PHPMAILER_METHOD_MAIL;
+$g_smtp_host = 'localhost';
