d60a025e772ce233902eee3839bb254aac982cef
Tatiana Maltseva <ext.tatyana.maltseva@tieto.com>
2016-11-17 17:39:14 +0300

    Тестовое задание № 21 Реализована интеграция тестов с баг-трекером MantisBT

---
+++ mantis-tests/src/test/java/ru/autoqa/mantis/model/Project.java
@@ -0,0 +1,24 @@
+package ru.autoqa.mantis.model;
+
+public class Project {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Project setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Project setId(int id) {
+        this.id = id;
+        return this;
+    }
+}
--- mantis-tests/src/test/java/ru/autoqa/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/autoqa/mantis/tests/TestBase.java
@@ -1,12 +1,22 @@
 package ru.autoqa.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.autoqa.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.Objects;
 
 public class TestBase {
 
@@ -24,6 +34,29 @@ public class TestBase {
     public void tearDown() throws IOException {
         app.stop();
         app.ftp().restore("config/config_inc.php.bak", "config/config_inc.php");
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = getMantisConnect();
+        IssueData issueData = mc.mc_issue_get(app.getAdminLogin(), app.getAdminPassword(), BigInteger.valueOf(issueId));
+        String name = issueData.getStatus().getName();
+        if (Objects.equals("resolved", name)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+        return new MantisConnectLocator()
+                .getMantisConnectPort(new URL(app.getProperty("web.baseURL")+"/api/soap/mantisconnect.php"));
+
     }
 
 }
